2f52d67259ccb9af0877f1153571b7205192d7ab
AYESHAZAD <39938138+AYESHAZAD@users.noreply.github.com>
2019-05-24 10:32:02 +0600

    Numbering Roads

---
+++ UVA 11723.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int r,n,c=0;
+
+    while(cin>>r>>n)
+    {
+
+        if(r==0&&n==0)
+            break;
+
+        int res=(r-1)/n;
+        c++;
+
+        if(res<=26)
+            printf("Case %d: %d\n",c,res);
+        else
+           printf("Case %d: impossible\n",c);
+    }
+
+    return 0;
+}
